a0af9dfaf55cba3d535d3c3db71e6d0f1aed5d1e
Philip Powers <philip_powers607@hotmail.com>
2021-10-06 11:51:53 -0400

    wrote query to return candidates

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const { urlencoded } = require('express');
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'ChrysPowers320',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 
 
 
 
 
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
